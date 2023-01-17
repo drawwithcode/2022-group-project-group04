d083424b9ed9c2421375c91d248fd9a3e66d37d2
elisabettacomo <elisabettacomo@outlook.it>
2023-01-17 16:38:13 +0100

    new hovering

--- 08-Consigli.js
+++ 08-Consigli.js
@@ -29,38 +29,6 @@ function setup() {
   myButton.mousePressed(nextPage);
   myButton.mouseOver(hovering);
 
-  sugg3 = createImg("./assets/int/s-3.svg");
-  sugg3.style("cursor:pointer;position:absolute");
-  sugg3.size(528 / 1.5, 723 / 1.5);
-  sugg3.position(140, height / 7);
-  sugg3.attribute("draggable", false);
-  sugg3.mouseOver(sugg3hover);
-  sugg3.mouseOut(sugg3over);
-
-  sugg2 = createImg("./assets/int/s-2.svg");
-  sugg2.style("cursor:pointer; ");
-  sugg2.size(542 / 1.5, 723 / 1.5);
-  sugg2.position(540, height / 7);
-  sugg2.attribute("draggable", false);
-  sugg2.mouseOver(sugg2hover);
-  sugg2.mouseOut(sugg2over);
-
-  sugg1 = createImg("./assets/int/s-1.svg");
-  sugg1.style("cursor:pointer; ");
-  sugg1.size(668 / 1.5, 476 / 1.5);
-  sugg1.position(950, height / 7);
-  sugg1.attribute("draggable", false);
-  sugg1.mouseOver(sugg1hover);
-  sugg1.mouseOut(sugg1over);
-
-  sugg4 = createImg("./assets/int/s-4.svg");
-  sugg4.style("cursor:pointer;");
-  sugg4.size(678 / 1.5, 213 / 1.5);
-  sugg4.position(950, height * 0.611);
-  sugg4.attribute("draggable", false);
-  sugg4.mouseOver(sugg4hover);
-  sugg4.mouseOut(sugg4over);
-
   img4 = createImg("./assets/images/suggest.svg");
   img4.style("position:absolute;");
   img4.size(413.4 / 2, 121 / 2);
@@ -95,50 +63,6 @@ function draw() {
 
 function hovering() {
   myButton.style("filter:invert(1)");
-}
-
-function sugg1hover() {
-  sugg1h = createImg("./assets/int/s-1-hover.svg");
-  sugg1h.style("cursor:pointer;");
-  sugg1h.size(668 / 1.5, 476 / 1.5);
-  sugg1h.position(950, height / 7);
-}
-
-function sugg1over() {
-  sugg1h.size(0, 0);
-}
-
-function sugg2hover() {
-  sugg2h = createImg("./assets/int/s-2-hover.svg");
-  sugg2h.style("cursor:pointer;");
-  sugg2h.size(542 / 1.5, 723 / 1.5);
-  sugg2h.position(540, height / 7);
-}
-
-function sugg2over() {
-  sugg2h.size(0, 0);
-}
-
-function sugg3hover() {
-  sugg3h = createImg("./assets/int/s-3-hover.svg");
-  sugg3h.style("cursor:pointer;position:absolute");
-  sugg3h.size(528 / 1.5, 723 / 1.5);
-  sugg3h.position(140, height / 7);
-}
-
-function sugg3over() {
-  sugg3h.size(0, 0);
-}
-
-function sugg4hover() {
-  sugg4h = createImg("./assets/int/s-4-hover.svg");
-  sugg4h.style("cursor:pointer;");
-  sugg4h.size(678 / 1.5, 213 / 1.5);
-  sugg4h.position(950, height * 0.611);
-}
-
-function sugg4over() {
-  sugg4h.size(0, 0);
 }
 
 function nextPage() {
